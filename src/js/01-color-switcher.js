04b1e1b1bfda0f9d26605d1cd7c3540bf2b455ef
vika512 <120404708+vika512@users.noreply.github.com>
2023-05-15 00:09:25 -0400

    added 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+const COLOR_CHANGE_TIME = 1000;
+let intervalID = null;
+
+console.log(body)
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+
+function onStart(evt) {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    intervalID = setInterval( () => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, COLOR_CHANGE_TIME)
+}
+
+function onStop() {
+    clearInterval(intervalID);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}

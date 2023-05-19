f62041a4c66ec05da2a0d2c6b9cd3cac63786c9d
vika512 <120404708+vika512@users.noreply.github.com>
2023-05-19 01:08:02 -0400

    added 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,63 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 
-// Notiflix.Notify.success('Sol lucet omnibus');
+// Notify.success(`Fulfilled promise ${position}` in ${delay}ms);
 
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
+// Notify.failure(`Rejected promise ${position}` in ${delay}ms);
+
+const refs = {
+  button: document.querySelector('button'),
+  form: document.querySelector('form'),
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name="step"]'),
+  amountInput: document.querySelector('input[name="amount"]'),
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  
+  return new Promise ((res, rej) => {
+    setTimeout (() => {
+      if (shouldResolve) {
+        res({ position, delay })
+      } else {
+        rej({ position, delay })
+      }
+    }, delay)
+  })
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const delay = Number(refs.delayInput.value);
+  const step = Number(refs.stepInput.value);
+  const amount = Number(refs.amountInput.value);
+  // console.log(delay, step, amount)
+
+
+  if (delay < 0 || step < 0 || amount < 0) {
+    Notify.warning(
+      `Enter number more than 0`
+    );
+  } else if (Number(amount) === 0) {
+    Notify.warning(`Enter number more than 0`);
   } else {
-    // Reject
+
+    for (let i = 0; i < amount; i++) {
+      createPromise(i, delay + step * i)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position + 1} in ${delay}ms`);
+        });
+    }
   }
+
 }
+
+
+
+
